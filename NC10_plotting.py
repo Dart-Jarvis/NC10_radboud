d7eadeba36538ff4d400612c32c9333835dac4f8
Dart-Jarvis <jiawen.li.gr@dartmouth.edu>
2025-09-03 17:40:19 -0600

    Ab initio

--- NC10_plotting.py
+++ NC10_plotting.py
@@ -29,12 +29,14 @@ T0=data[data['label']=='0'] # T0, tank gas and controls
 NC10=data[data['label']=='1'] # NC10
 NC10_ANME=data[data['label']=='2'] # NC10+ANME
 AeOM_P=data[data['label']=='P'] # Previous data from Li et al., 2024
+AeOM_P1=data[data['label']=='P1'] # Previous data from Krause et al., 2022
+AeOM_P2=data[data['label']=='P2'] # wang et al., 2016
 AOM_P=data[data['label']=='AOM_P'] # Previous data from Liu et al., 2023
 AeOM_m=AeOM_P[AeOM_P['f']>=0.4]
 
 # Create a mask to determine which series of data to lot
 mask={
-    "save_fig":0,
+    "save_fig":1,
     "T0": 1,
     "NC10":1,
     "NC10+ANME":1,
@@ -53,21 +55,26 @@ def quick_plot(ax,x,y,xerr,yerr,msk):
         ax.errorbar(T0[x],T0[y],xerr=T0[xerr],yerr=T0[yerr], markersize=40,label=r'T0', fmt='*', 
                     markerfacecolor='purple', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
     if msk["NC10"]==1:
-        ax.errorbar(NC10[x],NC10[y],xerr=NC10[xerr],yerr=NC10[yerr], markersize=16,label=r'NC10', fmt='o', 
+        ax.errorbar(NC10[x],NC10[y],xerr=NC10[xerr],yerr=NC10[yerr], markersize=24,label=r'NC10', fmt='o', 
                     markerfacecolor='orange', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
     if msk["NC10+ANME"]==1:
-        ax.errorbar(NC10_ANME[x],NC10_ANME[y],xerr=NC10_ANME[xerr],yerr=NC10_ANME[yerr], markersize=16,label=r'NC10+ANME', fmt='^', 
+        ax.errorbar(NC10_ANME[x],NC10_ANME[y],xerr=NC10_ANME[xerr],yerr=NC10_ANME[yerr], markersize=24,label=r'NC10+ANME', fmt='^', 
                     markerfacecolor='blue', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
     if msk["previous data"]==1:
-        ax.errorbar(AeOM_m[x],AeOM_m[y],xerr=AeOM_m[xerr],yerr=AeOM_m[yerr], markersize=12,label=r'AeOM (Li et al., 2024)', fmt='o', 
+        ax.errorbar(AeOM_m[x],AeOM_m[y],xerr=AeOM_m[xerr],yerr=AeOM_m[yerr], markersize=16,label=r'AeOM (Li et al., 2024)', fmt='o', 
                     markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=-1)
-        ax.errorbar(AOM_P[x],AOM_P[y],xerr=AOM_P[xerr],yerr=AOM_P[yerr], markersize=12,label=r'AOM (Liu et al., 2023)', fmt='s', 
+        ax.errorbar(AeOM_P1[x],AeOM_P1[y],xerr=AeOM_P1[xerr],yerr=AeOM_P1[yerr], markersize=16,label=r'AeOM (Krause et al., 2022)', fmt='^', 
+                    markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=-1)  
+        ax.errorbar(AeOM_P2[x],AeOM_P2[y],xerr=AeOM_P2[xerr],yerr=AeOM_P2[yerr], markersize=16,label=r'AeOM (Wang et al., 2016)', fmt='D', 
+                    markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=-1)          
+        ax.errorbar(AOM_P[x],AOM_P[y],xerr=AOM_P[xerr],yerr=AOM_P[yerr], markersize=16,label=r'AOM (Liu et al., 2023)', fmt='s', 
                     markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=-1)
+      
 
 quick_plot(ax1,"D13CH3D","D12CH2D2","cdse","ddse",mask)
 quick_plot(ax2,"d13C","dD","cse","dse",mask)
 
-ax1.legend(fontsize=24)
+ax1.legend(fontsize=20)
 ax1.set_xlabel('$\Delta^{13}$CH$_3$D (\u2030)', fontdict = font_labels)
 ax1.set_ylabel('$\Delta^{12}$CH$_2$D$_2$ (\u2030)', fontdict = font_labels)
 ax1.yaxis.set_minor_locator(MultipleLocator(2))
@@ -94,10 +101,10 @@ aCD_nc10=0.7843
 aDD_nc10=0.6296
 
 # sMMO ab initio model
-aC_smo_model=0.98247	
-aD_smo_model=0.70350
-aCD_smo_model=0.69051	
-aDD_smo_model=0.47326
+aC_smo_model=0.987583
+aD_smo_model=0.743150
+aCD_smo_model=0.733659	
+aDD_smo_model=0.512047
 
 # -41.381	0.011	-169.497	0.051	3.127	0.193	8.286	0.657
 dC0=-41.381
@@ -106,7 +113,7 @@ Dcd0=3.127
 Ddd0=8.286
 # mixing of two processes
 r=[0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
-f=np.arange(0.4,1.1,0.1)
+f=np.arange(0.2,1.1,0.1)
 # aC_mix=np.zeros(len(f))
 # aD_mix=np.zeros(len(f))
 # aCD_mix=np.zeros(len(f))
@@ -117,17 +124,8 @@ dC=np.zeros((len(r),len(f)))
 dD=np.zeros((len(r),len(f)))
 Dcd=np.zeros((len(r),len(f)))
 Ddd=np.zeros((len(r),len(f)))
-# Calculate the mixing
-# for i in range(len(f)):
-#     aC_mix[i]=aC_anme_hs*(1-f[i])+aC_nc10*f[i]
-#     aD_mix[i]=aD_anme_hs*(1-f[i])+aD_nc10*f[i]
-#     aCD_mix[i]=aCD_anme_hs*(1-f[i])+aCD_nc10*f[i]
-#     aDD_mix[i]=aDD_anme_hs*(1-f[i])+aDD_nc10*f[i]
-#     for j in range(len(r)):
-#         dC[i,j] = r[j]**(aC_mix[i]-1)*(dC0+1000)-1000
-#         dD[i,j] = r[j]**(aD_mix[i]-1)*(dD0+1000)-1000
-#         Dcd[i,j] = Dcd0+1000*(aCD_mix[i]-aC_mix[i]-aD_mix[i]+1)*np.log(r[j])
-#         Ddd[i,j] = Ddd0+1000*(aDD_mix[i]-2*aD_mix[i]+1)*np.log(r[j])
+# Results from ab initio calculations
+smmo_m=np.zeros((len(f),4))
 
 # Assume pMMO oxidized 1-f1 methane and mcr oxidize f1-f2 methane
 for i in range(len(r)):
@@ -142,7 +140,11 @@ for i in range(len(r)):
         Dcd[i,j] = Dcd0+1000*(aCD_anme_hs-aC_anme_hs-aD_anme_hs+1)*np.log(f1)+1000*(aCD_nc10-aC_nc10-aD_nc10+1)*np.log(f2)
         Ddd[i,j] = Ddd0+1000*(aDD_anme_hs-2*aD_anme_hs+1)*np.log(f1)+1000*(aDD_nc10-2*aD_nc10+1)*np.log(f2)
 
-
+for i in range(len(f)):
+    smmo_m[i,0]=(dC0+1000)*f[i]**(aC_smo_model-1)-1000 # Carbon isotope
+    smmo_m[i,1]=(dD0+1000)*f[i]**(aD_smo_model-1)-1000 # Hydrogen isotope
+    smmo_m[i,2]=Dcd0+1000*(aCD_smo_model-aC_smo_model-aD_smo_model+1)*np.log(f[i]) # 13CH3D
+    smmo_m[i,3]=Ddd0+1000*(aDD_smo_model-2*aD_smo_model+1)*np.log(f[i]) # 12CH2D2
 
 fig3,ax3=plt.subplots(figsize=(12,12))
 fig4,ax4=plt.subplots(figsize=(12,12))
@@ -150,42 +152,28 @@ fig4,ax4=plt.subplots(figsize=(12,12))
 # Clumped isotope 
 c=np.arange(0.2,1.1,0.1)
 ax3.plot(equib['D13CH3D'],equib['D12CH2D2'],'-k', label = 'Equilibrium', linewidth = 3.5, markersize = 15)
-for i in range(len(r)):
-    ax3.plot(Dcd[i,:],Ddd[i,:], 'ro--', linewidth=3.0, alpha=c[i])
-ax3.errorbar(T0['D13CH3D'],T0['D12CH2D2'],xerr=T0['cdse'],yerr=T0['ddse'], markersize=40,label=r'T0', fmt='*', 
-            markerfacecolor='purple', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax3.errorbar(NC10['D13CH3D'],NC10['D12CH2D2'],xerr=NC10['cdse'],yerr=NC10['ddse'], markersize=16,label=r'NC10', fmt='o', 
-            markerfacecolor='orange', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax3.errorbar(NC10_ANME['D13CH3D'],NC10_ANME['D12CH2D2'],xerr=NC10_ANME['cdse'],yerr=NC10_ANME['ddse'], markersize=16,label=r'NC10+ANME', fmt='^', 
-            markerfacecolor='blue', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax3.errorbar(AeOM_m['D13CH3D'],AeOM_m['D12CH2D2'],xerr=AeOM_m['cdse'],yerr=AeOM_m['ddse'], markersize=12,label=r'AeOM (Li et al., 2024)', fmt='o', 
-            markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=-1)
-ax3.errorbar(AOM_P['D13CH3D'],AOM_P['D12CH2D2'],xerr=AOM_P['cdse'],yerr=AOM_P['ddse'], markersize=12,label=r'AOM (Liu et al., 2023)', fmt='s', 
-            markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=-1)
-ax3.legend(fontsize=24)
+# for i in range(len(r)):
+#     ax3.plot(Dcd[i,:],Ddd[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+quick_plot(ax3,"D13CH3D","D12CH2D2","cdse","ddse",mask)
+ax3.plot(smmo_m[:,2],smmo_m[:,3], 'ro-',linewidth=4.0, label="Ab initio calculation")
+ax3.set_xlim([-5,10])
+ax3.set_ylim([-36,28])
+ax3.legend(fontsize=20)
 ax3.set_xlabel('$\Delta^{13}$CH$_3$D (\u2030)', fontdict = font_labels)
 ax3.set_ylabel('$\Delta^{12}$CH$_2$D$_2$ (\u2030)', fontdict = font_labels)
 ax3.yaxis.set_minor_locator(MultipleLocator(4))
-ax3.xaxis.set_minor_locator(MultipleLocator(2))
+ax3.xaxis.set_minor_locator(MultipleLocator(0.5))
 ax3.tick_params(which='major',direction='out', top=True, right=True, length=8, width=2.5, labelsize=32)
 ax3.tick_params(which='minor',direction='out', top=True, right=True, length=4, width=2.0, labelsize=32)
 # Bulk isotope
-for i in range(len(r)):
-    ax4.plot(dC[i,:],dD[i,:], 'ro--', linewidth=3.0, alpha=c[i])
-ax4.errorbar(T0['d13C'],T0['dD'],xerr=T0['cse'],yerr=T0['dse'], markersize=40,label=r'T0', fmt='*', 
-            markerfacecolor='purple', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax4.errorbar(NC10['d13C'],NC10['dD'],xerr=NC10['cse'],yerr=NC10['dse'], markersize=16,label=r'NC10', fmt='o', 
-            markerfacecolor='orange', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax4.errorbar(NC10_ANME['d13C'],NC10_ANME['dD'],xerr=NC10_ANME['cse'],yerr=NC10_ANME['dse'], markersize=16,label=r'NC10+ANME', fmt='^', 
-            markerfacecolor='blue', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax4.errorbar(AeOM_m['d13C'],AeOM_m['dD'],xerr=AeOM_m['cse'],yerr=AeOM_m['dse'], markersize=10,label=r'AeOM (Li et al., 2024)', fmt='o', 
-            markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=-1)
-ax4.errorbar(AOM_P['d13C'],AOM_P['dD'],xerr=AOM_P['cse'],yerr=AOM_P['dse'], markersize=12,label=r'AOM (Liu et al., 2023)', fmt='s', 
-            markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=-1)
+# for i in range(len(r)):
+#     ax4.plot(dC[i,:],dD[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+quick_plot(ax4,"d13C","dD","cse","dse",mask)
+ax4.plot(smmo_m[:,0],smmo_m[:,1], 'ro-',linewidth=4.0, label="Ab initio calculation")
 ax4.set_xlabel('$\delta^{13}$C (\u2030)', fontdict = font_labels)
 ax4.set_ylabel('$\delta$D (\u2030)', fontdict = font_labels)
 ax4.yaxis.set_minor_locator(MultipleLocator(20))
-ax4.xaxis.set_minor_locator(MultipleLocator(2))
+ax4.xaxis.set_minor_locator(MultipleLocator(1))
 ax4.tick_params(which='major',direction='out', top=True, right=True, length=8, width=2.5, labelsize=32)
 ax4.tick_params(which='minor',direction='out', top=True, right=True, length=4, width=2.0, labelsize=32)
 
@@ -195,64 +183,58 @@ fig6,ax6=plt.subplots(figsize=(12,6))
 fig7,ax7=plt.subplots(figsize=(12,6))
 fig8,ax8=plt.subplots(figsize=(12,6))
 
-ax5.errorbar(T0['f'],T0['D13CH3D'], xerr=T0['fse'], yerr=T0['cdse'], markersize=30,label=r'T0', fmt='*', 
-            markerfacecolor='purple', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax5.errorbar(NC10['f'],NC10['D13CH3D'], xerr=NC10['fse'], yerr=NC10['cdse'], markersize=16,label=r'NC10', fmt='o', 
-            markerfacecolor='orange', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax5.errorbar(NC10_ANME['f'],NC10_ANME['D13CH3D'], xerr=NC10_ANME['fse'], yerr=NC10_ANME['cdse'], markersize=16,label=r'NC10+ANME', fmt='^', 
-            markerfacecolor='blue', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax5.errorbar(AeOM_m['f'][AeOM_m['f']>=0.4],AeOM_m['D13CH3D'], xerr=AeOM_m['fse'], yerr=AeOM_m['cdse'], markersize=10,label=r'AeOM (Li et al., 2024)', fmt='s', 
-            markerfacecolor='gray', markeredgecolor='gray',markeredgewidth=2.5, ecolor='gray', elinewidth=2.5, zorder=-1)
-for i in range(len(r)):
-    ax5.plot(f,Dcd[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+def quick_plot_f(ax,y,ye,msk):
+    if msk["T0"]==1:
+        ax.errorbar(T0['f'],T0[y], xerr=T0['fse'], yerr=T0[ye], markersize=30,label=r'T0', fmt='*', 
+                    markerfacecolor='purple', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
+    if msk["NC10"]==1:
+        ax.errorbar(NC10['f'],NC10[y], xerr=NC10['fse'], yerr=NC10[ye], markersize=24,label=r'NC10', fmt='o', 
+                    markerfacecolor='orange', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
+    if msk["NC10+ANME"]==1:
+        ax.errorbar(NC10_ANME['f'],NC10_ANME[y], xerr=NC10_ANME['fse'], yerr=NC10_ANME[ye], markersize=24,label=r'NC10+ANME', fmt='^', 
+                    markerfacecolor='blue', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
+    if msk["previous data"]==1:
+        ax.errorbar(AeOM_m['f'][AeOM_m['f']>=0.4],AeOM_m[y], xerr=AeOM_m['fse'], yerr=AeOM_m[ye], markersize=16,label=r'AeOM (Li et al., 2024)', fmt='s', 
+                    markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='gray', elinewidth=2.5, zorder=-1)
+        ax.errorbar(AeOM_P1['f'],AeOM_P1[y], xerr=AeOM_P1['fse'], yerr=AeOM_P1[ye], markersize=16,label=r'AeOM (Krause et al., 2022)', fmt='^', 
+                    markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='gray', elinewidth=2.5, zorder=-1)
+        ax.errorbar(AeOM_P2['f'],AeOM_P2[y], xerr=AeOM_P2['fse'], yerr=AeOM_P2[ye], markersize=16,label=r'AeOM (Wang et al., 2016)', fmt='D', 
+                    markerfacecolor='white', markeredgecolor='black',markeredgewidth=2.5, ecolor='gray', elinewidth=2.5, zorder=-1)
+
+quick_plot_f(ax5,"D13CH3D","cdse",mask)
+# for i in range(len(r)):
+#     ax5.plot(f,Dcd[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+ax5.plot(f,smmo_m[:,2],'ro--',linewidth=4.0)
 ax5.axes.invert_xaxis()
 ax5.set_ylabel('$\Delta^{13}$CH$_3$D (\u2030)', fontdict = font_labels)
 ax5.set_xlabel(r'$f$', fontdict = font_labels)
 ax5.tick_params(which='major',direction='out', top=True, right=True, length=8, width=2.5, labelsize=32)
 ax5.tick_params(which='minor',direction='out', top=True, right=True, length=4, width=2.0, labelsize=32)
 
-ax6.errorbar(T0['f'],T0['D12CH2D2'], xerr=T0['fse'], yerr=T0['ddse'], markersize=30,label=r'T0', fmt='*', 
-            markerfacecolor='purple', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax6.errorbar(NC10['f'],NC10['D12CH2D2'], xerr=NC10['fse'], yerr=NC10['ddse'], markersize=16,label=r'NC10', fmt='o', 
-            markerfacecolor='orange', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax6.errorbar(NC10_ANME['f'],NC10_ANME['D12CH2D2'], xerr=NC10_ANME['fse'], yerr=NC10_ANME['ddse'], markersize=16,label=r'NC10+ANME', fmt='^', 
-            markerfacecolor='blue', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax6.errorbar(AeOM_m['f'][AeOM_m['f']>=0.4],AeOM_m['D12CH2D2'], xerr=AeOM_m['fse'], yerr=AeOM_m['ddse'], markersize=10,label=r'AeOM (Li et al., 2024)', fmt='s', 
-            markerfacecolor='gray', markeredgecolor='gray',markeredgewidth=2.5, ecolor='gray', elinewidth=2.5, zorder=-1)
-for i in range(len(r)):
-    ax6.plot(f,Ddd[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+quick_plot_f(ax6,"D12CH2D2",'ddse',mask)
+ax6.plot(f,smmo_m[:,3],'ro--',linewidth=4.0)
+# for i in range(len(r)):
+#     ax6.plot(f,Ddd[i,:], 'ro--', linewidth=3.0, alpha=c[i])
 ax6.axes.invert_xaxis()
 ax6.set_ylabel('$\Delta^{12}$CH$_2$D$_2$ (\u2030)', fontdict = font_labels)
 ax6.set_xlabel(r'$f$', fontdict = font_labels)
 ax6.tick_params(which='major',direction='out', top=True, right=True, length=8, width=2.5, labelsize=32)
 ax6.tick_params(which='minor',direction='out', top=True, right=True, length=4, width=2.0, labelsize=32)
 
-ax7.errorbar(T0['f'],T0['d13C'], xerr=T0['fse'], yerr=T0['cse'], markersize=30,label=r'T0', fmt='*', 
-            markerfacecolor='purple', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax7.errorbar(NC10['f'],NC10['d13C'], xerr=NC10['fse'], yerr=NC10['cse'], markersize=16,label=r'NC10', fmt='o', 
-            markerfacecolor='orange', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax7.errorbar(NC10_ANME['f'],NC10_ANME['d13C'], xerr=NC10_ANME['fse'], yerr=NC10_ANME['cse'], markersize=16,label=r'NC10+ANME', fmt='^', 
-            markerfacecolor='blue', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax7.errorbar(AeOM_m['f'][AeOM_m['f']>=0.4],AeOM_m['d13C'], xerr=AeOM_m['fse'], yerr=AeOM_m['cse'], markersize=10,label=r'AeOM (Li et al., 2024)', fmt='s', 
-            markerfacecolor='gray', markeredgecolor='gray',markeredgewidth=2.5, ecolor='gray', elinewidth=2.5, zorder=-1)
-for i in range(len(r)):
-    ax7.plot(f,dC[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+quick_plot_f(ax7,"d13C","cse",mask)
+# for i in range(len(r)):
+#     ax7.plot(f,dC[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+ax7.plot(f,smmo_m[:,0],'ro--',linewidth=4.0)
 ax7.axes.invert_xaxis()
 ax7.set_ylabel('$\delta^{13}$C (\u2030)', fontdict = font_labels)
 ax7.set_xlabel(r'$f$', fontdict = font_labels)
 ax7.tick_params(which='major',direction='out', top=True, right=True, length=8, width=2.5, labelsize=32)
 ax7.tick_params(which='minor',direction='out', top=True, right=True, length=4, width=2.0, labelsize=32)
 
-ax8.errorbar(T0['f'],T0['dD'], xerr=T0['fse'], yerr=T0['dse'], markersize=30,label=r'T0', fmt='*', 
-            markerfacecolor='purple', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax8.errorbar(NC10['f'],NC10['dD'], xerr=NC10['fse'], yerr=NC10['dse'], markersize=16,label=r'NC10', fmt='o', 
-            markerfacecolor='orange', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax8.errorbar(NC10_ANME['f'],NC10_ANME['dD'], xerr=NC10_ANME['fse'], yerr=NC10_ANME['dse'], markersize=16,label=r'NC10+ANME', fmt='^', 
-            markerfacecolor='blue', markeredgecolor='black',markeredgewidth=2.5, ecolor='black', elinewidth=2.5, zorder=2)
-ax8.errorbar(AeOM_m['f'][AeOM_m['f']>=0.4],AeOM_m['dD'], xerr=AeOM_m['fse'], yerr=AeOM_m['dse'], markersize=10,label=r'AeOM (Li et al., 2024)', fmt='s', 
-            markerfacecolor='gray', markeredgecolor='gray',markeredgewidth=2.5, ecolor='gray', elinewidth=2.5, zorder=-1)
-for i in range(len(r)):
-    ax8.plot(f,dD[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+quick_plot_f(ax8,"dD","dse",mask)
+# for i in range(len(r)):
+#     ax8.plot(f,dD[i,:], 'ro--', linewidth=3.0, alpha=c[i])
+ax8.plot(f,smmo_m[:,1],'ro--',linewidth=4.0)
 ax8.axes.invert_xaxis()
 ax8.set_ylabel('$\delta$D (\u2030)', fontdict = font_labels)
 ax8.set_xlabel(r'$f$', fontdict = font_labels)
--- ab initio.py
+++ ab initio.py
@@ -35,40 +35,48 @@ def read_freq(df,iso_name):
 ts_freq=read_freq(ts_freq_df,iso)
 ch4_freq=read_freq(ch4_freq_df,iso_ch4)
 
-T=273.15+25 # Temperature in K
+T=273.15+30 # Temperature in K
 # Calculate the partition function from Lasaga 1991, GRL
-def calc_rpf(freq, T):
+def calc_rpf(freq, T, img):
     rpf=np.zeros(freq.shape[1])
     c = 2.99792458e10   # speed of light in cm/s
-    h=6.626*10**(-34) # Plank constant, J/s
+    h=6.62607015*10**(-34) # Plank constant, J/s
     kb=1.380649*10**(-23) # Boltzmann constant, J/K
     for i in range(freq.shape[1]):
         rpf_temp=1.0
-        for j in range(1,freq.shape[0]): 
-            # Make sure to rule out the imaginary frequency (Mode 1) when calculating rpf
-            nu = freq[j, i] * c  # convert to Hz
-            rpf_temp=rpf_temp*h*nu/(kb*T)/(1-math.exp((-h*nu)/(kb*T)))
+        if img==1:
+            for j in range(1,freq.shape[0]): 
+                # Make sure to rule out the imaginary frequency (Mode 1) when calculating rpf
+                nu = freq[j, i] * c  # convert to Hz
+                rpf_temp=rpf_temp*h*nu/(kb*T)/(1-math.exp((-h*nu)/(kb*T)))
+        elif img==0:
+            for j in range(freq.shape[0]): 
+                # No imaginary frequency for CH4 molecule
+                nu = freq[j, i] * c  # convert to Hz
+                rpf_temp=rpf_temp*h*nu/(kb*T)/(1-math.exp((-h*nu)/(kb*T)))
+
         rpf[i]=rpf_temp
     return rpf
 
 # Calculate the KIE between two isotopologues, iso2 is normally CH4, iso2=0;
 # iso3 and 4 are for CH4 isotopologues, iso4 is normally CH4, iso4=0
 def calc_rpfr(iso1,iso2,iso3,iso4): 
-    h=6.626*10**(-34) # Plank constant, J/s
+    h=6.62607015*10**(-34) # Plank constant, J/s
     c= 2.99792458e10   # speed of light in cm/s
-    kb=1.380649*10**(-23) # Boltzmann constant, J/K 
+    kb=1.380649*10**(-23) # Boltzmann constant, J/K
+    # R=8.314  # Ideal gas constant J/(mol*K)
     # Identify the imaginary frequency ratio
     img_freq_ratio=ts_freq[0,iso1]/ts_freq[0,iso2]
     # Calculate the tunneling factor, set it at 1 now
     tun=1.0
     # Calculate delta E, make sure to convert the frequency to Hz first
-    deltaE=0.5*h*c*sum(ts_freq[:,iso1])-0.5*h*c*sum(ts_freq[:,iso2])-(0.5*h*c*sum(ch4_freq[:,iso3])-0.5*h*c*sum(ch4_freq[:,iso4]))
+    deltaE=0.5*h*c*sum(ts_freq[1:,iso1])-0.5*h*c*sum(ts_freq[1:,iso2])-(0.5*h*c*sum(ch4_freq[:,iso3])-0.5*h*c*sum(ch4_freq[:,iso4]))
     # Calculate RPFR
     rpfr=img_freq_ratio*tun*ts_rpf[iso1]/ts_rpf[iso2]*ch4_rpf[iso4]/ch4_rpf[iso3]*math.exp(-deltaE/(kb*T))
     return rpfr
 
-ts_rpf=calc_rpf(ts_freq,T)
-ch4_rpf=calc_rpf(ch4_freq,T)
+ts_rpf=calc_rpf(ts_freq,T,1) # Has imaginary frequency for transition state
+ch4_rpf=calc_rpf(ch4_freq,T,0) # No imaginary frequency for ch4 molecule
 
 # Calculate carbon isotope fractionation
 a13=calc_rpfr(16,0,5,0) # 0.9713 at 27 oC, from Li et al., 2024, GCA
@@ -78,5 +86,6 @@ aQH3D=1/4*(calc_rpfr(17,0,6,0)+calc_rpfr(18,0,6,0)
            +calc_rpfr(19,0,6,0)+calc_rpfr(20,0,6,0))
 aCH2D2=1/6*(calc_rpfr(5,0,2,0)+calc_rpfr(6,0,2,0)+calc_rpfr(7,0,2,0)
             +calc_rpfr(8,0,2,0)+calc_rpfr(9,0,2,0)+calc_rpfr(10,0,2,0))
+aCD4=calc_rpfr(31,0,9,0)
 
-print(a13,"\n",a2H, "\n", aQH3D, "\n", aCH2D2)
+print(a13,"\n",a2H, "\n", aQH3D, "\n", aCH2D2, "\n", aCD4)
