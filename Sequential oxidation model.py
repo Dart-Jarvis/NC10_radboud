a8a2d4c26af958cb97c20848e244b52609eda96a
Dart-Jarvis <jiawen.li.gr@dartmouth.edu>
2025-09-28 15:30:17 -0600

    som

--- Sequential oxidation model.py
+++ Sequential oxidation model.py
@@ -20,7 +20,7 @@ a_nc10=[0.9778, 0.8019, 0.7843, 0.6296]
 
 #INPUTS
 nmolec=10000
-num=10000  #number of time steps
+num=100000  #number of time steps
 #------------------------------------------------------------------------------------------------
 t_lower=0.0001 # minimum time for time interval
 t_upper= 1.0 # maximum time for time interval, dimensionless
@@ -31,9 +31,9 @@ TK=T+273.15 # Equilibration temperature of initial methane in K
 
 output=False
 
-r_aeom=1.0 # Relative rate of AeOM
+r_aeom=0.8 # Relative rate of AeOM
 r_aom=1.0-r_aeom
-os=True # Open system or not
+os=False # Open system or not
 if os==True:
     t_upper=10000.0 # Change the t_upper for open system to make sure it reaches steady state
 # Define a transport flux
@@ -62,7 +62,7 @@ def dfdt(t,Y):
     Kt= (K_aeom[0]+K_aom[0])*Y[0]+(K_aeom[5]+K_aom[5])*Y[1]+(K_aeom[1]+K_aom[1]+K_aeom[2]+K_aom[2])*Y[2]+(K_aeom[6]+K_aom[6]+K_aeom[7]+K_aom[7])*Y[3]+(K_aeom[3]+K_aom[3]+K_aeom[4]+K_aom[4])*Y[4] 
     print("Total methane oxidized:", Kt)
     print("Total CH4:", tCH4)
-    phi=0.5
+    phi=0.4
     Kin=Kt/phi
     Kout=Kin-Kt
     if os==False:
